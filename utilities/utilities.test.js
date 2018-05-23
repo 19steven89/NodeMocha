32304cd2f96100f360ff8d58147b16a8ca06c7be
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-05-23 23:01:45 +0100

    added more tests using node mocha framework. also added a test for asynchronous functions using the setTimeout method in a callback function.

--- utilities/utilities.test.js
+++ utilities/utilities.test.js
@@ -1,17 +1,63 @@
+const expect = require("expect");
 const utils = require("./utilities.js")
 
 it("should add 2 numbers", () => {
   var res = utils.add(33, 11);
-
-  if(res !== 44){
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  //used the asertion method expect from the npm expect
+  expect(res).toBe(44).toBeA("number");
 });
+
+//added param in callback function to make mocha wait for the test to complete
+//before running. Otherwise the function is skipped since it wouldnt wait the 1000ms time.
+//done() forces app to wait, then test is completed properly
+it("should async add a number", (done) => {
+    utils.asyncAdd(4,7, (sum) => {
+      expect(sum).toBe(11).toBeA("number");
+      done();
+    });
+});
+
+
+it("should async square a number", (done) => {
+  utils.asyncSquare(4, (sum) => {
+    expect(sum).toBe(16).toBeA("number");
+    done();
+  });
+});
+
 
 it("should square number", () => {
   var res1 = utils.square(9, 9);
+  expect(res1).toBe(81).toBeA("number");
+});
 
-  if(res1 !== 81){
-    throw new Error(`Expected 81, but got ${res1}`)
-  }
+it("should expect some values", () => {
+  //expect(12).toNotBe(11);
+  //use toEqual to compare objects as toBe does not work which compares object equality
+  //expect({name: "steven"}).toEqual({name: "steven"});
+  expect([2,3,4]).toInclude(3);
+  expect([2,3,4]).toExclude(6);
+
+  expect({
+    name: "steven",
+    age: 29,
+    location: "Glasgow"
+  }).toInclude({age: 29})
+
+});
+
+it("should set first and last names", () => {
+  var user = {
+    age: 29,
+    location: "Glasgow"
+  };
+
+  var answer = utils.setName(user, "Steven Barry");
+  expect(answer).toEqual(user);
+  expect(answer).toInclude({
+    firstName: "Steven",
+    lastName: "Barry",
+    age: 29,
+    location: "Glasgow"
+  }).toBeA("object");
 });
